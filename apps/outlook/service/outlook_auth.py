c15d01a90d9c748942da8315c51da1b49bd426e3
Croissant (Mohammad) <118067995+Javanitalab@users.noreply.github.com>
2024-05-13 19:16:02 +0200

    first commit

---
+++ apps/outlook/service/outlook_auth.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+
+class OutlookAuth():
+
+    def create_authorization_uri(self):
+        client_secret_path = os.path.join('.', 'data', 'microsoft', 'entra_config.json')
+        data = dict()
+        with open(client_secret_path, 'r') as file:
+            data = json.load(file)
+
+        url = f"https://login.microsoftonline.com/common/oauth2/v2.0/authorize/?client_id={data['client_id']}&response_type=code&redirect_uri={self.domain}&scope={self.scope}"
